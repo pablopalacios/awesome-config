028974a9165bafcc6c096c67197689c54166e60b
Palácios <ppalacios992@gmail.com>
2018-07-04 23:03:48 -0300

    added urxvt as terminal appliaction

--- rc.lua
+++ rc.lua
@@ -44,7 +44,7 @@ end
 beautiful.init(gears.filesystem.get_configuration_dir() .. "theme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
